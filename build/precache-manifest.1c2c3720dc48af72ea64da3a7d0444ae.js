e7a2ded04dcc321448acf91d1a5a71b0e06c1a44
Jorge M <jkmvz5@gmail.com>
2020-02-18 11:59:43 -0500

    Aplicacion Demo Medi Seda

---
+++ build/precache-manifest.1c2c3720dc48af72ea64da3a7d0444ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61168ebb5a9ca4861092f8dd1f460aa7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "741da1ae3aaa6bc2137f",
+    "url": "/static/css/main.8c938fea.chunk.css"
+  },
+  {
+    "revision": "ffa0cb72ec98d1e2956d",
+    "url": "/static/js/2.601bdf23.chunk.js"
+  },
+  {
+    "revision": "5ab660b2fbbad84135d612366df4ebf7",
+    "url": "/static/js/2.601bdf23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "741da1ae3aaa6bc2137f",
+    "url": "/static/js/main.a7e3003a.chunk.js"
+  },
+  {
+    "revision": "19a332c98d282bfb4549",
+    "url": "/static/js/runtime-main.9dcc8a99.js"
+  }
+]);
